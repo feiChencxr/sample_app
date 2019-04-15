b87bbf647fc6c98dcf7cdfdebcbf325ddab7f1b1
tenmind <tenmind@localhost.localdomain>
2019-04-15 01:34:01 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
-# describe User do
-
-# 	before { @user = User.new(name: "Example User", email: "user@example.com") }
-
-# 	subject { @user }
-
-# 	it { should respond_to(:name) }
-# 	it { should respond_to(:email) }
-
-#pending "add some examples to (or delete) #{__FILE__}"
 
 describe User do
     before do
@@ -25,6 +15,8 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
 
 
     it { should be_valid }
@@ -112,6 +104,8 @@ describe User do
 
     end
 
-
-
+    describe "remember token" do
+        before { @user.save }
+        it(:remember_token) { should_not be_blank }  #its error  it right
+    end
 end
